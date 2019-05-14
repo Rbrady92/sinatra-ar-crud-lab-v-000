548ddbff98ad510b383d9f0da7ddee9c2a55ea21
Ryan Brady <ryanbrady369@gmail.com>
2019-05-14 23:28:19 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,56 +8,51 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # get '/' do
-  #   @articles = Article.all
-  #
-  #   erb :index
-  # end
-
-  get '/articles/new' do
-    erb :new
-  end
-
-  post '/articles' do
-    @new_article = Article.new(title: params[:title], content: params[:content])
-    @new_article.save
-
-    redirect to '/articles/:id'
-  end
-
-  get 'articles' do
+  get '/' do 
+    redirect to '/articles'
+  end 
+ 
+  get '/articles' do
     @articles = Article.all
-
     erb :index
   end
-
-  get 'articles/:id' do
-    @article = Article.find_by(id: params[:id])
-
+  
+   get '/articles/new' do
+    erb :new
+  end
+  
+  post '/articles' do
+    @article = Article.create(title: params[:title], content: params[:content])
+    redirect to "/articles/#{@article.id}"
+  end   
+  
+  get '/articles/:id' do 
+    
+    @article = Article.find(params[:id])
     erb :show
-  end
-
-  get '/articles/:id/edit' do
-    @article = Article.find_by(id: params[:id])
-    erb :edit
-  end
-
-  patch 'articles/:id' do
-    #Article.update(params[:id], title: params[:title], content: params[:content])
-    @article.find_by(id: params[:id])
-
+  end 
+  
+  patch '/articles/:id' do 
+    @article = Article.find(params[:id])
     @article.title = params[:title]
     @article.content = params[:content]
     @article.save
-
-    erb :show
+  
+    redirect to "/articles/#{@article.id}"
   end
-
-  delete 'articles/:id' do
-    @article = Article.find_by_id(params[:id])
-    @article.delete
-
-    erb :show
-  end
+  
+  get '/articles/:id/edit' do 
+    @article = Article.find(params[:id])
+    erb :edit
+  end   
+  
+  
+  
+  delete '/articles/:id' do 
+    @article = Article.find(params[:id])
+    @article.destroy
+    
+    erb :articles
+  end 
 
 end
