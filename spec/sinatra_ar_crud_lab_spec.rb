0b937cfc1d184ea8a191cbf563e974fc08f175cc
Ryan Brady <ryanbrady369@gmail.com>
2019-05-14 22:58:28 +0000

    Automatically backed up by Learn

--- spec/sinatra_ar_crud_lab_spec.rb
+++ spec/sinatra_ar_crud_lab_spec.rb
@@ -5,7 +5,7 @@ describe "Magazine App" do
   let(:article_content) { "This is my first article!!!" }
 
   before do
-    @article1 = Article.create(:title => 'article_title', :content => 'article_content')
+    @article1 = Article.create(:title => article_title, :content => article_content)
     @article2 = Article.create(:title => "second article", :content => "I'm a really good writer")
   end
 
