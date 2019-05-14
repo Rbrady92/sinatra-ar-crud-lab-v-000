a63ccc25deaf1acf2671e424f46dc75763cb4f6b
Ryan Brady <ryanbrady369@gmail.com>
2019-05-14 23:46:39 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/articles' do
-    @article=Article.create(params)
+    @article = Article.create(params)
 
     redirect "/articles/#{@article.id}"
   end
